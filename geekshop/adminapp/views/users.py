1c2cb91965f97dcfa7eb2ae226d5ab42e617e96d
Cher-Study <mainsail_645@inbox.ru>
2022-02-08 21:59:37 +0300

    admin: read

---
+++ geekshop/adminapp/views/users.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from authapp.models import ShopUser
+
+
+def user_create(request):
+    pass
+
+
+def users(request):
+    users = ShopUser.objects.all().order_by('id')
+
+    return render(request, 'adminapp/user/users.html', context={
+        'title': 'Пользователи',
+        'objects': users,
+    })
+
+
+def user_update(request):
+    pass
+
+
+def user_delete(request):
+    pass
