a171b44ee223347a6da9fb1a9452e9e8327e6a6e
Cher-Study <mainsail_645@inbox.ru>
2022-02-23 15:07:18 +0300

    make signals to create profiles

--- geekshop/authapp/models.py
+++ geekshop/authapp/models.py
@@ -2,6 +2,8 @@ from datetime import timedelta
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.timezone import now
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -19,3 +21,26 @@ class ShopUser(AbstractUser):
     activation_key = models.CharField(max_length=128, blank=True)
     activation_key_expires = models.DateTimeField(
         default=get_activation_key_expiration_date)
+
+
+class ShopUserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+
+    GENDER_CHOISES = (
+        (MALE, 'Мужской'),
+        (FEMALE, 'Женский'),
+    )
+
+    user = models.OneToOneField(
+        ShopUser, unique=True, null=False, db_index=True, on_delete=models.CASCADE, related_name='profile')
+    about = models.TextField(verbose_name='о себе', max_length=512, blank=True)
+    gender = models.CharField(
+        verbose_name='пол', choices=GENDER_CHOISES, max_length=1, blank=True)
+
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+        else:
+            instance.profile.save()
