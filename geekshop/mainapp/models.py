3f52f5bdc083f45f16159d487d3870b568702f0e
Cher-Study <mainsail_645@inbox.ru>
2022-03-28 16:20:07 +0300

    fixed and done

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -1,5 +1,5 @@
-from email.policy import default
-from msilib.schema import Media
+# from email.policy import default
+# from msilib.schema import Media
 from django.db import models
 
 # Create your models here.
